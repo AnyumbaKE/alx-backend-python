17a6ec8225d489d9e3348bb4c810fa3808b1c5d4
AnyumbaKE <esanyumba@gmail.com>
2024-04-18 20:59:49 +0000

    1. Basic annotations - concat

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
-"""A type-annotated function concat"""
+'''Task 1's module.
+'''
 
 
 def concat(str1: str, str2: str) -> str:
+    '''Concatenates two strings.
+    '''
     return str1 + str2
